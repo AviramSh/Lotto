792b25b95dfb7f5898dc3fe1837a5f3558efa5ac
Aviram Sharabi <aviram@sharabi.co.il>
2017-01-13 18:16:15 +0200

    Main class - new User Start.

--- src/lotto/Lotto.java
+++ src/lotto/Lotto.java
@@ -5,6 +5,9 @@
  */
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author AVIRAM
@@ -16,6 +19,22 @@ public class Lotto {
      */
     public static void main(String[] args) {
       
+        int[] myTicket= new int[7];
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Lotto 6 numbers : ");
+        
+        for(int i=0;i<6;i++)
+            input.nextInt(myTicket[i]);
+        
+        System.out.println("Enter strong number : ");
+        input.nextInt(myTicket[6]);
+        
+        Row myRow = new Row();
+        
+       myRow.setNumber(myTicket);
+       myRow.setStrongNumber(myTicket[7]);
     }
+    
     
 }
